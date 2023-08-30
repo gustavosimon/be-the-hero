2eb43966c57276f9c77bf5eea8712885a6075dbb
Gustavo Simon <gustavo.andre.simon@hotmail.com>
2023-08-29 22:44:09 -0300

    Ajuste de código novamente.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,4 +4,11 @@ const routes = require("./routes");
 const { errors } = require("celebrate");
 const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
 module.exports = app;
